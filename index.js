d047603f90a0cf8bb2b0ec4634d3d35687f5caee
DmirtyPronin <36396347+ProninDP@users.noreply.github.com>
2020-03-05 14:36:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const readlineSync = require('readline-sync');
-var enemy = require("./monster.js");
-var hero = require("./mag.js");
-let cooldownMag = 4;
-let cooldownMonster = 4;
-function movesMonster() {
+var enemy = require("./monster.js"); //параметры монстра
+var hero = require("./mag.js"); //параметры мага
+let cooldownMag = 4; //очки действий
+let cooldownMonster = 4; //очки действий
+function movesMonster() { //ходы монстра
   let moves = [];
   while(true){
     if(cooldownMonster > 0){
-      let move = Math.floor(Math.random() * 3);
+      let move = Math.floor(Math.random() * 3); //генерируем случайное действие
         if(enemy.monster.moves[move].cooldown <= cooldownMonster){
           moves.push(move);
           cooldownMonster -= enemy.monster.moves[move].cooldown;
           console.log(`Компьютер делает ход: ${enemy.monster.moves[move].name}`);
-          if(moves.length==3){
+          if(moves.length==1){ //сколько действий за ход можно сделать пока есть очки хода
             return moves;
           }
         }
@@ -21,7 +21,7 @@ function movesMonster() {
     }
   }
 }
-function movesGamer() {
+function movesGamer() { //ходы игрока
   let moves = [];
   while(true){
     if(cooldownMag > 0){
@@ -30,12 +30,12 @@ function movesGamer() {
           console.log(`Доступные действия: ${key} - ${hero.mag.moves[key].name}`);
         }
       }
-      let move = readlineSync.questionInt('Делайте ход: ');
+      let move = readlineSync.questionInt('Делайте ход: '); //запрос на действие
       if(hero.mag.moves[move].cooldown <= cooldownMag){
         moves.push(move);
         cooldownMag -= hero.mag.moves[move].cooldown;
         console.log(`Ход игрока: ${hero.mag.moves[move].name}`);
-        if(moves.length==3){
+        if(moves.length==1){ //сколько действий за ход можно сделать пока есть очки хода
           return moves;
         }
       }
@@ -44,13 +44,14 @@ function movesGamer() {
     }
   }
 }
-function game(health){
+function game(health){ //функция просчитывания хода игры
     let mag_health = health;
     let monster_health = enemy.monster.maxHealth;
   while(true){
       if (mag_health > 0 && monster_health > 0){
       let monster_moves = movesMonster();
       let mag_moves = movesGamer();
+      // временные параметры для суммирования действий
       var temp_monster = {  "physicalDmg": 0, // физический урон
                             "magicDmg": 0,    // магический урон
                             "physicArmorPercents": 0, // физическая броня
@@ -61,6 +62,7 @@ function game(health){
                         "physicArmorPercents": 0,
                         "magicArmorPercents": 0
       };
+      //суммирование действий за ход
       for(let move of monster_moves){
         temp_monster.physicalDmg += enemy.monster.moves[move].physicalDmg;
         temp_monster.magicDmg += enemy.monster.moves[move].magicDmg;
@@ -73,12 +75,13 @@ function game(health){
         temp_mag.physicArmorPercents += hero.mag.moves[move].physicArmorPercents;
         temp_mag.magicArmorPercents += hero.mag.moves[move].magicArmorPercents;
       }
-          mag_health = mag_health - (temp_monster.physicalDmg - ((temp_monster.physicalDmg/100)*temp_mag.physicArmorPercents) + temp_monster.magicDmg - ((temp_monster.magicDmg/100)*temp_mag.magicArmorPercents));
-          monster_health = monster_health - (temp_mag.physicalDmg - ((temp_mag.physicalDmg/100)*temp_monster.physicArmorPercents) + temp_mag.magicDmg - ((temp_mag.magicDmg/100)*temp_monster.magicArmorPercents));
+      //вычисляем полученый урон, конец хода
+      mag_health = mag_health - (temp_monster.physicalDmg - ((temp_monster.physicalDmg/100)*temp_mag.physicArmorPercents) + temp_monster.magicDmg - ((temp_monster.magicDmg/100)*temp_mag.magicArmorPercents));
+      monster_health = monster_health - (temp_mag.physicalDmg - ((temp_mag.physicalDmg/100)*temp_monster.physicArmorPercents) + temp_mag.magicDmg - ((temp_mag.magicDmg/100)*temp_monster.magicArmorPercents));
       cooldownMonster += 1;
-          cooldownMag += 1;
-          console.log(`Осталось жизней у Вас: ${mag_health}`);
-          console.log(`Осталось жизней у врага: ${monster_health}`);
+      cooldownMag += 1;
+      console.log(`Осталось жизней у Вас: ${mag_health}`);
+      console.log(`Осталось жизней у врага: ${monster_health}`);
     }else{
           if (mag_health > 0) {
             console.log('Вы победили!');
